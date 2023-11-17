948c40623b358e8abb579b3b3c4b4faac2c97745
shom <shomayev.i@gmail.com>
2023-11-17 20:40:55 +0600

    refactor + added abi

--- vote.sol
+++ vote.sol
@@ -4,39 +4,53 @@ pragma solidity >=0.7 <0.9.0;
 contract Voting {
 
     struct Candidate {
-        address addr;
         string name;
-        uint8 votes;
+        uint32 votes;
         bool isRegistered;
     }
     
+
     mapping(address => Candidate) candidates;
     address[] registered;
 
-    function register(string memory name, address addr) public {
-        if (candidates[addr].isRegistered) {
-            revert("already registered");
-        }
-        candidates[msg.sender] = Candidate(msg.sender, name, 10, true);
+    function register(string memory name) public {
+        // regirster and recieve 10 votes
+        require(!candidates[msg.sender].isRegistered, "address already exists");
+        candidates[msg.sender] = Candidate(name, 10, true);
         registered.push(msg.sender);
     }
 
-    function vote(address voteFor) public {
-        if (voteFor == msg.sender) {
-            revert("cannot give vote to yourself");
-        }
-        if (!candidates[msg.sender].isRegistered) {
-            revert("you have to register first");
-        }
-        if (candidates[msg.sender].votes >= 0) {
-            revert("no more votes left");
-        }
+    function giveVote(address to) public {
+        // give 1 vote to other candidate
+        require(to != msg.sender, "cannot give to yourself");
+        require(candidates[msg.sender].isRegistered, "you have to register first");
+        require(candidates[to].isRegistered, "candidate is not registered");
+        require(candidates[msg.sender].votes >= 0, "no more votes left");
+
         candidates[msg.sender].votes -= 1;
-        candidates[voteFor].votes += 1;
+        candidates[to].votes += 1;
     }
 
-    functoin getCandidates() public returns address[]
-        return registered
+    function mintVotes() public {
+        // gives 10 votes to caller
+        require(candidates[msg.sender].isRegistered, "you have to register first");
+        candidates[msg.sender].votes += 10;
+    }
 
+    function burnVotes() public {
+        // burns 10 votes
+        require(candidates[msg.sender].isRegistered, "you have to register first");
+        require(candidates[msg.sender].votes >= 10, "not enough votes to burn");
+        candidates[msg.sender].votes -= 10;
+    }
+
+    function getCandidate(address addr) public view returns(Candidate memory) {
+        require(candidates[addr].isRegistered, "candidate is not registered");
+        return candidates[addr];
+    }
+
+    function getAllRegistered() public view returns (address[] memory) {
+        return registered;
+    }
 
 }
